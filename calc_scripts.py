3bbdb07a6d05aef1052fbd7b9d9a4090353f0f66
kozlyuk <sergey.kozlyuk@gmail.com>
2018-06-15 15:29:25 +0300

    Planner 1.8

--- calc_scripts.py
+++ calc_scripts.py
@@ -3,20 +3,20 @@
 
 def gks(deal, tasks):
 
-    message = '<html><body>Калькуляція по договору {} від {}:<br><br>' \
+    report = '<html><body>Калькуляція по договору {} від {}:<br><br>' \
               .format(deal, deal.date.strftime('%d.%m.%Y'))
 
     objects = tasks.values('object_code', 'object_address').distinct()
 
-    message += '<table border="1">' \
+    report += '<table border="1">' \
                '<tr><th>Шифр об’єкту</th><th>Адреса об’єкту</th></tr>'
     for obj in objects:
-        message += '<tr><td align="center">{}</td><td>{}</td></tr>' \
+        report += '<tr><td align="center">{}</td><td>{}</td></tr>' \
                    .format(obj['object_code'], obj['object_address'])
-    message += '</table><br>'
+    report += '</table><br>'
 
-    message += '<table border="1">\
-               <tr><th>№ п/п</th><th>Назва об’єкту та вид робіт</th><th>Кількість</th>\
+    report += '<table border="1"> \
+               <tr><th>№ п/п</th><th>Назва об’єкту та вид робіт</th><th>Кількість</th> \
                <th>Ціна, з ПДВ  грн.</th><th>Вартість, з ПДВ грн.</th></tr>'
 
     project_types = tasks.values('project_type__price_code', 'project_type__description', 'project_type__price') \
@@ -28,32 +28,32 @@ def gks(deal, tasks):
         if ptype['project_type__price'] != 0:
             index += 1
             object_codes = tasks.filter(project_type__price_code=ptype['project_type__price_code']) \
-                .values_list('object_code', flat=True)
+                                .values_list('object_code', flat=True)
             object_list = ''
             for obj in object_codes:
                 object_list += obj + ' '
             count = object_codes.count()
             value = ptype['project_type__price'] * count
             svalue += value
-            message += '<tr align="center">\
-                               <td>{}</td><td align="left">{} {}</td><td>{}</td><td>{}</td><td>{}</td>\
-                               </tr>' \
-                .format(index, ptype['project_type__description'], object_list,
-                        count, ptype['project_type__price'], value)
+            report += '<tr align="center"> \
+                       <td>{}</td><td align="left">{} {}</td><td>{}</td><td>{}</td><td>{}</td> \
+                       </tr>' \
+                       .format(index, ptype['project_type__description'], object_list,
+                               count, ptype['project_type__price'], value)
 
-    woVAT = round(svalue / 6 * 5, 2)
-    VAT = round(svalue / 6, 2)
-    message += '<tr align="center"><td colspan="2" rowspan="3">Загальна вартість</td>\
-                           <td colspan="3">без ПДВ: {} грн. {} коп.</td></tr>\
-                           <tr align="center"><td colspan="3">ПДВ: {} грн. {} коп.</td></tr>\
-                           <tr align="center"><td colspan="3">з ПДВ: {} грн. {} коп.</td></tr>' \
-        .format(int(woVAT), int((woVAT - int(woVAT)) * 100),
-                int(VAT), int((VAT - int(VAT)) * 100),
-                int(svalue), int((svalue - int(svalue)) * 100))
+    wovat = round(svalue / 6 * 5, 2)
+    vat = round(svalue / 6, 2)
+    report += '<tr align="center"><td colspan="2" rowspan="3">Загальна вартість</td>\
+               <td colspan="3">без ПДВ: {} грн. {} коп.</td></tr>\
+               <tr align="center"><td colspan="3">ПДВ: {} грн. {} коп.</td></tr>\
+               <tr align="center"><td colspan="3">з ПДВ: {} грн. {} коп.</td></tr>' \
+               .format(int(wovat), int((wovat - int(wovat)) * 100),
+                       int(vat), int((vat - int(vat)) * 100),
+                       int(svalue), int((svalue - int(svalue)) * 100))
 
-    message += '</table>'
+    report += '</table>'
 
-    return message
+    return report
 
 
 def msz(deal, tasks):
@@ -62,11 +62,18 @@ def msz(deal, tasks):
 
     objects = tasks.values('object_code', 'object_address').distinct()
 
+    report += '<table border="1"> \
+              <tr><th>Шифр об’єкту</th><th>Адреса об’єкту</th></tr>'
+    for obj in objects:
+        report += '<tr><td align="center">{}</td><td>{}</td></tr>' \
+                  .format(obj['object_code'], obj['object_address'])
+    report += '</table><br>'
+
     report += '<table border="1">\
                                <tr><th>№ п/п</th><th>Найменування робіт</th><th>Однинця виміру</th><th>Кількість</th>\
                                <th>Ціна за одиницю без ПДВ, грн.</th><th>Вартість без ПДВ, грн.</th></tr>'
     obj_index = 0
-    swoVAT = 0
+    swovat = 0
     count = 1
     unit = 'шт.'
 
@@ -81,23 +88,23 @@ def msz(deal, tasks):
 
         for task in object_tasks:
             task_index += 1
-            woVAT = round(task['project_type__price'] / 6 * 5, 2)
-            swoVAT += woVAT
+            wovat = task['project_type__price'] / 6 * 5
+            swovat += wovat
 
             report += '<tr align="center"> \
                       <td>{}.{}</td><td align="left">{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td> \
                       </tr>' \
                       .format(obj_index, task_index, task['project_type__description'],
-                      unit, count, woVAT, woVAT*count)
+                      unit, count, round(wovat, 2), round(wovat*count, 2))
 
-    VAT = round(swoVAT / 6, 2)
-    swVAT = round(swoVAT / 5 * 6, 2)
-    report += '<tr><td colspan="5" align="right">Загальна вартість без ПДВ:</td><td align="center">{} грн. {} коп.</td></tr> \
-              <tr><td colspan="5" align="right">ПДВ 20%:</td><td align="center">{} грн. {} коп.</td></tr> \
-              <tr><td colspan="5" align="right">Всього з ПДВ:</td><td align="center">{} грн. {} коп.</td></tr>' \
-              .format(int(swoVAT), int((swoVAT - int(swoVAT)) * 100),
+    VAT = round(swovat / 6, 2)
+    swvat = round(swovat / 5 * 6, 2)
+    report += '<tr><td colspan="4" align="right">Загальна вартість без ПДВ:</td><td colspan="2" align="center">{} грн. {} коп.</td></tr> \
+              <tr><td colspan="4" align="right">ПДВ 20%:</td><td colspan="2" align="center">{} грн. {} коп.</td></tr> \
+              <tr><th colspan="4" align="right">Всього з ПДВ:</th><td colspan="2" align="center">{} грн. {} коп.</td></tr>' \
+              .format(int(swovat), int((swovat - int(swovat)) * 100),
                     int(VAT), int((VAT - int(VAT)) * 100),
-                    int(swVAT), int((swVAT - int(swVAT)) * 100))
+                    int(swvat), int((swvat - int(swvat)) * 100))
 
     report += '</table>'
 
