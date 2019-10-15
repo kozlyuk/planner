5f67c14b3b79c582d9c69f5324b77abb4e9b3fd1
Sergey <sergey.kozlyuk@gmail.com>
2019-10-15 18:34:39 +0300

    Created Analytics app.

--- analytics/models.py
+++ analytics/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+
 from planner.models import Employee, Task
 
 
@@ -15,3 +17,27 @@ class Bonus(models.Model):
 
     def __str__(self):
         return self.employee.name
+
+
+class KpiName(models.Model):
+    name = models.CharField('Назва показника', max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'Показник ефективності'
+        verbose_name_plural = 'Показники ефективності'
+
+    def __str__(self):
+        return self.name
+
+
+class Kpi(models.Model):
+    name = models.ForeignKey(KpiName, verbose_name='Показник', on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField('Значення', validators=[MaxValueValidator(100)])
+    created = models.DateField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'КПЕ'
+
+    def __str__(self):
+        return self.name
