48850f97974bde37536829e401bac6fba847b920
Sergey <sergey.kozlyuk@gmail.com>
2019-10-28 13:45:29 +0200

    Added task for saving daily bonuses of Employees.

---
+++ analytics/tasks.py
@@ -0,0 +1,46 @@
+from planner.celery import app
+from celery.utils.log import get_task_logger
+from datetime import datetime
+from django.db.models import Q, Sum
+
+from analytics.models import Bonus
+from planner.models import Employee, Task, IntTask
+
+logger = get_task_logger(__name__)
+
+
+@app.task
+def save_bonus():
+    """ Save daily bonuses of Employees """
+    employees = Employee.objects.all()
+    
+    for employee in employees:
+        bonuses = 0
+
+        # executor bonuses
+        executions = employee.execution_set.filter(Q(task__exec_status=Task.Done) |
+                                                   Q(task__exec_status=Task.Sent),
+                                                   part__gt=0,
+                                                   task__actual_finish__month=datetime.now().month,
+                                                   task__actual_finish__year=datetime.now().year)
+        for query in executions:
+            bonuses += query.task.exec_bonus(query.part)
+    
+        # owner bonuses
+        tasks = employee.task_set.filter(exec_status=Task.Sent,
+                                         actual_finish__month=datetime.now().month,
+                                         actual_finish__year=datetime.now().year)
+        for query in tasks:
+            bonuses += query.owner_bonus()
+
+        # inttask bonuses
+        bonuses += employee.inttask_set.filter(exec_status=IntTask.Done,
+                                               actual_finish__month=datetime.now().month,
+                                               actual_finish__year=datetime.now().year)\
+                                       .aggregate(Sum('bonus'))
+
+        # save bonuses
+        bonus = Bonus(employee=employee, value=bonuses)
+        bonus.save()
+        
+    logger.info("Employee bonuses for {}.{} saved".format(datetime.now().month, datetime.now().year))
