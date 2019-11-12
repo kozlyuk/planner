11170f858b653090eacc2ee91e3d4da0c7e8b14d
arrathilar <wervvolf@gmail.com>
2019-11-12 10:51:10 +0200

    fix sprint filter form

--- planner/forms.py
+++ planner/forms.py
@@ -267,14 +267,8 @@ class SprintFilterForm(forms.Form):
     owner = forms.ChoiceField(label='Керівник проекту', required=False,
                               widget=forms.Select(attrs={"onChange": 'submit()'}))
     customer = forms.ChoiceField(label='Замовник', required=False, widget=forms.Select(attrs={"onChange": 'submit()'}))
-#    start_date = forms.DateField(label='Дата початку', widget=AdminDateWidget(),
-#                                 initial=date.today() + timedelta((0-date.today().weekday()) % 7 - 7))
-#    finish_date = forms.DateField(label='Дата закінчення', widget=AdminDateWidget(),
-#                                  initial=date.today() + timedelta((0-date.today().weekday()) % 7 - 2))
-    data_range = forms.DateField(label='Період', widget=forms.DateInput(attrs={"class": "air-datepicker",
-                                                                               "data-range": "true",
-                                                                               "data-multiple-dates-separator": " - ",
-                                                                               "autocomplete": "off"}))
+    start_date = forms.DateField(label='Дата початку', widget=forms.DateInput(attrs={"class": "air-datepicker-start", "autocomplete": "off"}))
+    finish_date = forms.DateField(label='Дата закінчення', widget=forms.DateInput(attrs={"class": "air-datepicker-finish", "autocomplete": "off"}))
 
 
 class TaskForm(forms.ModelForm):
--- planner/views.py
+++ planner/views.py
@@ -793,8 +793,8 @@ class SprintTaskList(ListView):
         exec_status = self.request.GET.get('exec_status', '0')
         owner = self.request.GET.get('owner', '0')
         customer = self.request.GET.get('customer', '0')
-#        start_date = self.request.GET.get('start_date')
-#        finish_date = self.request.GET.get('finish_date')
+        start_date = self.request.GET.get('start_date')
+        finish_date = self.request.GET.get('finish_date')
         data_range = self.request.GET.get('data_range')
         order = self.request.GET.get('o', '0')
 
@@ -804,13 +804,13 @@ class SprintTaskList(ListView):
             tasks = tasks.filter(owner=owner)
         if customer != '0':
             tasks = tasks.filter(deal__customer=customer)
-#        if start_date:
-#            tasks = tasks.filter(planned_start__gte=start_date)
-#        if finish_date:
-#            tasks = tasks.filter(planned_finish__lte=finish_date)
+        if start_date:
+            tasks = tasks.filter(planned_start__gte=start_date)
+        if finish_date:
+            tasks = tasks.filter(planned_finish__lte=finish_date)
         if data_range:
-            start_date = datetime.strptime(data_range[:10], '%d.%m.%Y')
-            finish_date = datetime.strptime(data_range[13:], '%d.%m.%Y')
+            start_date = datetime.strftime(data_range[:10], '%d.%m.%Y')
+            finish_date = datetime.strftime(data_range[13:], '%d.%m.%Y')
             tasks = tasks.filter(planned_start__gte=start_date, planned_finish__lte=finish_date)
         if order != '0':
             tasks = tasks.order_by(order)
@@ -820,9 +820,10 @@ class SprintTaskList(ListView):
 
     def get_context_data(self, **kwargs):
         start_date = date.today() + timedelta((0-date.today().weekday()) % 7 - 7)
-        finish_date = date.today() + timedelta((0-date.today().weekday()) % 7 - 2)
+        # finish_date = date.today() + timedelta((0-date.today().weekday()) % 7 - 2)
         context = super(SprintTaskList, self).get_context_data(**kwargs)
-        context['date_range'] = start_date.strptime('%d.%m.%Y') + ' - ' + finish_date.strptime('%d.%m.%Y')
+        context['start_date'] = start_date.strftime('%d.%m.%Y')
+        # context['finish_date'] = finish_date.strftime('%d.%m.%Y')
         context['tasks_count'] = Task.objects.all().count()
         context['form_action'] = reverse('sprint_list')
         context['tasks_filtered'] = self.get_queryset().count()
