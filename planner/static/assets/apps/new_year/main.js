3431c3a62df5af5823241339525d12498afc9f0b
Arrathilar <Wervvolf@gmail.com>
2019-12-26 16:02:01 +0200

    Hotfix (#80)
    * act_status  PartlyIssued fixed
    
    * fix load prioritet
    
    * fix print button
    
    * query_string dispatching in listfilters added
    
    * Changed deal to deal.number in sprint_list.html
    
    * Search in deal.number added to SprintList
    
    * Added need_project_code field to Project
    
    * Auto set project_code on creating tasks
    
    * fix new_year_script
    
    * Migration from Charfield to Integerfield
    
    * fix: button position in task detail
    
    Co-authored-by: kozlyuk <sergey.kozlyuk@gmail.com>

--- planner/static/assets/apps/new_year/main.js
+++ planner/static/assets/apps/new_year/main.js
@@ -22,7 +22,7 @@ particlesJS("particles-js", {
           "nb_sides": 5
         },
         "image": {
-          "src": "http://www.dynamicdigital.us/wp-content/uploads/2013/02/starburst_white_300_drop_2.png",
+          "src": "/static/assets/apps/new_year/starburst_white_300_drop_2.png",
           "width": 100,
           "height": 100
         }
