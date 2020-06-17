506622436fe23663eac336fe629aaa5f104a619a
arrathilar <wervvolf@gmail.com>
2020-06-17 16:49:56 +0300

    fix actual finish

--- planner/views.py
+++ planner/views.py
@@ -118,7 +118,7 @@ class BonusesCalc(TemplateView):
             index += 1
             executions_list.append([index, ex.task.object_code, ex.task.object_address,
                                     ex.task.project_type, ex.part_name, ex.part,
-                                    round(ex.task.exec_bonus(ex.part), 2), ex.finish_date])
+                                    round(ex.task.exec_bonus(ex.part), 2), ex.task.actual_finish])
             bonuses += ex.task.exec_bonus(ex.part)
 
         # get inttasks
