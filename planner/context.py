d8eab7cf693da6784445e8b7ab3d0ca2f69eaf20
Sergey Kozlyuk <sergey.kozlyuk@gmail.com>
2021-09-07 12:02:57 +0300

    Fix date.today() issue

--- planner/context.py
+++ planner/context.py
@@ -10,9 +10,6 @@ from analytics.models import Kpi
 from eventlog.models import Log
 
 
-TODAY = date.today()
-
-
 def executors_rating():
     """ Get list of executors ratings in descending ordering """
     period = date.today().replace(day=1)
@@ -24,23 +21,23 @@ def context_general(user):
     user_inttasks = IntTask.objects.filter(executor__user=user)
     active_inttasks = user_inttasks.exclude(exec_status=IntTask.Done)
     hd_inttasks_count = user_inttasks.filter(exec_status=IntTask.Done,
-                                             actual_finish__month=TODAY.month,
-                                             actual_finish__year=TODAY.year).count()
+                                             actual_finish__month=date.today().month,
+                                             actual_finish__year=date.today().year).count()
     active_inttasks_count = active_inttasks.count()
-    overdue_inttasks_count = active_inttasks.exclude(planned_finish__gte=TODAY).count()
+    overdue_inttasks_count = active_inttasks.exclude(planned_finish__gte=date.today()).count()
     overdue_inttasks_div = int(overdue_inttasks_count / (active_inttasks_count + hd_inttasks_count) * 100) \
         if active_inttasks_count > 0 else 0
 
     bonus = Kpi.objects.filter(employee__user=user,
                                name__in=[Kpi.BonusItel, Kpi.BonusGKP, Kpi.BonusSIA],
-                               period__month=TODAY.month,
-                               period__year=TODAY.year)\
+                               period__month=date.today().month,
+                               period__year=date.today().year)\
                        .aggregate(Sum('value'))['value__sum'] or 0
     try:
         inttask_bonus = Kpi.objects.get(employee__user=user,
                                         name=Kpi.Tasks,
-                                        period__month=TODAY.month,
-                                        period__year=TODAY.year)\
+                                        period__month=date.today().month,
+                                        period__year=date.today().year)\
                                    .value
     except Kpi.DoesNotExist:
         inttask_bonus = 0
@@ -48,14 +45,14 @@ def context_general(user):
     try:
         productivity = Kpi.objects.get(employee__user=user,
                                        name=Kpi.Productivity,
-                                       period__month=TODAY.month,
-                                       period__year=TODAY.year)\
+                                       period__month=date.today().month,
+                                       period__year=date.today().year)\
                                    .value
     except Kpi.DoesNotExist:
         productivity = 0
 
-    news = News.objects.exclude(actual_from__gt=TODAY).exclude(
-        actual_to__lte=TODAY).order_by('-created')
+    news = News.objects.exclude(actual_from__gt=date.today()).exclude(
+        actual_to__lte=date.today()).order_by('-created')
     events = Event.objects.filter(
         next_date__isnull=False).order_by('next_date')
     if user.is_superuser:
@@ -94,9 +91,9 @@ def context_accounter(user):
                         .exclude(number__icontains='загальний')
     debtor_deals = unpaid_deals.filter(exec_status=Deal.Sent)\
                                .exclude(pay_date__isnull=True) \
-                               .exclude(pay_date__gte=TODAY)
+                               .exclude(pay_date__gte=date.today())
     overdue_deals = deals.exclude(exec_status=Deal.Sent) \
-                         .exclude(expire_date__gte=TODAY) \
+                         .exclude(expire_date__gte=date.today()) \
                          .exclude(number__icontains='загальний')
 
     active_deals_count = active_deals.count()
@@ -142,8 +139,8 @@ def context_pm(user):
     hd_tasks = owner_tasks.filter(exec_status=Task.Done)
     hd_tasks_count = hd_tasks.count()
     sent_tasks = owner_tasks.filter(exec_status=Task.Sent,
-                                    actual_finish__month=TODAY.month,
-                                    actual_finish__year=TODAY.year)
+                                    actual_finish__month=date.today().month,
+                                    actual_finish__year=date.today().year)
     sent_tasks_count = sent_tasks.count()
 
     # Progress bar section
@@ -151,10 +148,10 @@ def context_pm(user):
     active_tasks_count = active_tasks.count()
     tasks_div = int(sent_tasks_count / active_tasks_count * 100) \
         if active_tasks_count > 0 else 0
-    overdue_tasks_count = active_tasks.exclude(deal__expire_date__gte=TODAY,
+    overdue_tasks_count = active_tasks.exclude(deal__expire_date__gte=date.today(),
                                                planned_finish__isnull=True)\
-                                      .exclude(deal__expire_date__gte=TODAY,
-                                               planned_finish__gte=TODAY)\
+                                      .exclude(deal__expire_date__gte=date.today(),
+                                               planned_finish__gte=date.today())\
                                       .count()
     overdue_tasks_div = int(
         overdue_tasks_count / active_tasks_count * 100) if active_tasks_count > 0 else 0
@@ -186,8 +183,8 @@ def context_projector(user):
     ip_executions = user_executions.filter(exec_status=Execution.InProgress)
     oc_executions = user_executions.filter(exec_status=Execution.OnChecking)
     hd_executions = user_executions.filter(exec_status=Execution.Done,
-                                           finish_date__month=TODAY.month,
-                                           finish_date__year=TODAY.year)
+                                           finish_date__month=date.today().month,
+                                           finish_date__year=date.today().year)
     td_executions_count = td_executions.count()
     ip_executions_count = ip_executions.count()
     oc_executions_count = oc_executions.count()
@@ -199,10 +196,10 @@ def context_projector(user):
     executions_div = int(hd_executions_count / active_executions_count *
                          100) if active_executions_count > 0 else 0
     overdue_executions_count = user_executions.exclude(exec_status=Execution.Done)\
-                                              .exclude(task__deal__expire_date__gte=TODAY,
+                                              .exclude(task__deal__expire_date__gte=date.today(),
                                                        task__planned_finish__isnull=True)\
-                                              .exclude(task__deal__expire_date__gte=TODAY,
-                                                       task__planned_finish__gte=TODAY)\
+                                              .exclude(task__deal__expire_date__gte=date.today(),
+                                                       task__planned_finish__gte=date.today())\
                                               .count()
     overdue_executions_div = int(overdue_executions_count / active_executions_count * 100) \
         if active_executions_count > 0 else 0
