1ca89401250cd9d880533c17f0aa3eddb69fe40e
Sergey Kozlyuk <sergey.kozlyuk@gmail.com>
2021-12-13 09:49:01 +0200

    Snow speed update

--- planner/static/assets/apps/new_year/main.js
+++ planner/static/assets/apps/new_year/main.js
@@ -56,7 +56,7 @@ particlesJS("particles-js", {
     },
     "move": {
       "enable": true,
-      "speed": 2,
+      "speed": 5,
       "direction": "bottom",
       "random": true,
       "straight": false,
