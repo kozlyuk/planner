1918fe281c1bcd00399176b0a4339c16000785b8
Sergey Kozlyuk <sergey.kozlyuk@gmail.com>
2023-04-05 11:55:23 +0300

    Fix wrong AdvancePaid status

--- planner/models.py
+++ planner/models.py
@@ -1187,11 +1187,13 @@ class Order(ModelDiffMixin, models.Model):
                 return self.AdvanceApproved
             if self.approved_date and payment_sum >= self.advance:
                 return self.Approved
+            if payment_sum >= self.advance:
+                return self.AdvancePaid
         else:
             if self.approved_date:
                 return self.Approved
-        if payment_sum >= self.advance:
-            return self.AdvancePaid
+            if payment_sum > 0:
+                return self.AdvancePaid
         return self.NotPaid
 
     def can_approve(self):
